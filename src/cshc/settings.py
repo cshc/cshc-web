6346c99e6d176bdfbb5cd20d6823eeb54140da22
Graham McCulloch <admin@grahammcculloch.co.uk>
2017-10-01 15:01:04 +0700

    Added responsive admin theme 'Django Jet'

--- src/cshc/settings.py
+++ src/cshc/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'venues.apps.VenuesConfig',
+    'jet',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- src/cshc/urls.py
+++ src/cshc/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^venues/', include('venues.urls')),
+    url(r'^jet/', include('jet.urls', 'jet')),  # Django JET URLS
     url(r'^admin/', admin.site.urls),
 ]
