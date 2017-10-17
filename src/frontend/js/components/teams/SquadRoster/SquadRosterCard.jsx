25e7e693e1f23e705554bd1b0ca888944c55de59
Graham McCulloch <admin@grahammcculloch.co.uk>
2017-10-17 18:14:08 +0700

    Improvements to Squad Roster card

--- src/frontend/js/components/teams/SquadRoster/SquadRosterCard.jsx
+++ src/frontend/js/components/teams/SquadRoster/SquadRosterCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { VictoryPie } from 'victory';
+import { VictoryPie, VictoryTooltip } from 'victory';
 import Member from 'models/member';
 import ProgressBar from 'components/common/ProgressBar';
 import IconList from 'components/common/IconList';
@@ -28,30 +28,41 @@ const SquadRosterCard = ({ memberStats, teamTotals }) => {
   return (
     <div className="col-md-6 col-lg-3 g-mb-30">
       <article className="g-bg-white">
-        <figure className={styles.imageContainer}>
-          <div className={styles.imageWrapper}>
-            <img className={styles.image} src={imgSrc} alt="Member profile pic" />
-            <div className={`${styles.imageOverlayFront} g-pa-10`}>
-              <IconList
-                className={styles.awardRow}
-                iconClass="fa fa-star g-mr-5"
-                count={memberStats.mom}
-              />
-              <IconList
-                className={styles.awardRow}
-                iconClass="fa fa-lemon-o g-mr-5"
-                count={memberStats.lom}
-              />
+        <div className={styles.imageFlipContainer}>
+          <figure className={styles.imageContainer}>
+            <div className={styles.front}>
+              <div className={styles.frontContent}>
+                <img className={styles.image} src={imgSrc} alt="Member profile pic" />
+                <div className={styles.imageOverlayFront}>
+                  <IconList
+                    className={styles.awardRow}
+                    iconClass="fa fa-star g-mr-5"
+                    count={memberStats.mom}
+                  />
+                  <IconList
+                    className={`${styles.awardRow} g-pb-10`}
+                    iconClass="fa fa-lemon-o g-mr-5"
+                    count={memberStats.lom}
+                  />
+                  {(memberStats.isCaptain || memberStats.isViceCaptain) && (
+                    <div className={styles.captain}>
+                      {memberStats.isCaptain ? 'Captain' : 'Vice Captain'}
+                    </div>
+                  )}
+                </div>
+              </div>
             </div>
-          </div>
-          <div className={styles.imageOverlayBack}>
-            <span
-              className={`${styles.shirt} shirt-number shirt-number-2xl ${memberStats.member.gender.toLowerCase()}`}
-            >
-              {memberStats.member.shirtNumber}
-            </span>
-          </div>
-        </figure>
+            <div className={styles.back}>
+              <div className={styles.backContent}>
+                <span
+                  className={`${styles.shirt} shirt-number shirt-number-2xl ${memberStats.member.gender.toLowerCase()}`}
+                >
+                  {memberStats.member.shirtNumber}
+                </span>
+              </div>
+            </div>
+          </figure>
+        </div>
         <div className="u-shadow-v24 g-pa-25">
           <h3 className="h5 g-mb-5 text-center">{Member.fullName(memberStats.member)}</h3>
           <p className="text-center g-mb-5">
@@ -66,16 +77,15 @@ const SquadRosterCard = ({ memberStats, teamTotals }) => {
             color="blue"
           />
           <div className={styles.flexRow}>
-            <svg height="70" width="70" viewBox="45 45 60 60">
-              <VictoryPie
-                standalone={false}
-                colorScale={wldColors}
-                data={wldData}
-                height={150}
-                width={150}
-                labels={() => null}
-              />
-            </svg>
+            <VictoryPie
+              colorScale={wldColors}
+              data={wldData}
+              height={150}
+              width={150}
+              padding={10}
+              labelRadius={40}
+              labels={d => d.x}
+            />
             <a
               className="btn btn-md u-btn-primary g-color-primary--hover g-bg-white--hover g-font-weight-600 g-font-size-11 text-uppercase"
               href="#"
