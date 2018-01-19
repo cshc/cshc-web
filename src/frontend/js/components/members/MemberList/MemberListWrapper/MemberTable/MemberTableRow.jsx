dd5a883f1d1cffffeedf3c3fc312731157f6e37f
Graham McCulloch <admin@grahammcculloch.co.uk>
2018-01-19 11:04:20 +0700

    Add link to member detail from member list

--- src/frontend/js/components/members/MemberList/MemberListWrapper/MemberTable/MemberTableRow.jsx
+++ src/frontend/js/components/members/MemberList/MemberListWrapper/MemberTable/MemberTableRow.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Urls from 'util/urls';
 
 const MemberTableRow = ({ member }) => (
-  <tr>
+  <tr className="g-cursor-pointer" title="View member details..." onClick={() => { window.location = Urls.member_detail(member.id); }}>
     <td>{member.firstName}</td>
     <td>{member.lastName}</td>
     <td className="priority3">{member.gender[0]}</td>
@@ -17,6 +18,7 @@ const MemberTableRow = ({ member }) => (
 
 MemberTableRow.propTypes = {
   member: PropTypes.shape({
+    id: PropTypes.string,
     first_name: PropTypes.string,
     last_name: PropTypes.string,
   }).isRequired,
