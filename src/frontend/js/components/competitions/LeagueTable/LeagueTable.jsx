13f517de470a3a98931415ad3053e7411c5c181f
Graham McCulloch <admin@grahammcculloch.co.uk>
2018-01-24 15:33:19 +0700

    League Table bugfix

--- src/frontend/js/components/competitions/LeagueTable/LeagueTable.jsx
+++ src/frontend/js/components/competitions/LeagueTable/LeagueTable.jsx
@@ -7,68 +7,82 @@ import Urls from 'util/urls';
  * 
  * Teams are either Opposition.Team or Team.ClubTeam instances.
  */
-const LeagueTable = ({ data, teamName }) => (
-  <div className="table-responsive">
-    <table className="table table-sm table-hover">
-      <thead>
-        <tr>
-          <th>Team</th>
-          <th>
-            <abbr title="Played">P</abbr>
-          </th>
-          <th>
-            <abbr title="Won">W</abbr>
-          </th>
-          <th>
-            <abbr title="Drawn">D</abbr>
-          </th>
-          <th>
-            <abbr title="Lost">L</abbr>
-          </th>
-          <th>
-            <abbr title="Goals For">GF</abbr>
-          </th>
-          <th>
-            <abbr title="Goals Against">GA</abbr>
-          </th>
-          <th>
-            <abbr title="Goal Difference">GD</abbr>
-          </th>
-          <th>
-            <abbr title="Points">Pts</abbr>
-          </th>
-          <th>&nbsp;</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.results.map((row) => {
-          const isOurTeam = row.teamName === teamName;
-          const rowClass = isOurTeam ? 'table-success' : '';
-          return (
-            <tr key={row.teamName} className={rowClass}>
-              <td>
-                {isOurTeam ? (
-                  row.teamName
-                ) : (
-                  <a href={Urls.opposition_club_detail(row.oppTeam.club.slug)}>{row.teamName}</a>
-                )}
-              </td>
-              <td>{row.played}</td>
-              <td>{row.won}</td>
-              <td>{row.drawn}</td>
-              <td>{row.lost}</td>
-              <td>{row.goalsFor}</td>
-              <td>{row.goalsAgainst}</td>
-              <td>{row.goalDifference}</td>
-              <td>{row.points}</td>
-              <td>{row.notes}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
-  </div>
-);
+const LeagueTable = ({ data, teamName }) => {
+  const teamNameCell = (row, isCSHCTeam, isOurTeam) => {
+    if (isCSHCTeam) {
+      return isOurTeam ? (
+        row.teamName
+      ) : (
+        <a href={Urls.clubteam_detail(row.ourTeam.slug)} title="Team Details">
+          {row.teamName}
+        </a>
+      );
+    }
+    return (
+      <a href={Urls.opposition_club_detail(row.oppTeam.club.slug)} title="Club Details">
+        {row.teamName}
+      </a>
+    );
+  };
+
+  return (
+    <div className="table-responsive">
+      <table className="table table-sm table-hover">
+        <thead>
+          <tr>
+            <th>Team</th>
+            <th>
+              <abbr title="Played">P</abbr>
+            </th>
+            <th>
+              <abbr title="Won">W</abbr>
+            </th>
+            <th>
+              <abbr title="Drawn">D</abbr>
+            </th>
+            <th>
+              <abbr title="Lost">L</abbr>
+            </th>
+            <th>
+              <abbr title="Goals For">GF</abbr>
+            </th>
+            <th>
+              <abbr title="Goals Against">GA</abbr>
+            </th>
+            <th>
+              <abbr title="Goal Difference">GD</abbr>
+            </th>
+            <th>
+              <abbr title="Points">Pts</abbr>
+            </th>
+            <th>&nbsp;</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.results.map((row) => {
+            const isCSHCTeam = row.teamName.startsWith('Cambridge South');
+            const isOurTeam = row.teamName === teamName;
+            const rowClass = isOurTeam ? 'table-success' : '';
+            return (
+              <tr key={row.teamName} className={rowClass}>
+                <td>{teamNameCell(row, isCSHCTeam, isOurTeam)}</td>
+                <td>{row.played}</td>
+                <td>{row.won}</td>
+                <td>{row.drawn}</td>
+                <td>{row.lost}</td>
+                <td>{row.goalsFor}</td>
+                <td>{row.goalsAgainst}</td>
+                <td>{row.goalDifference}</td>
+                <td>{row.points}</td>
+                <td>{row.notes}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 LeagueTable.propTypes = {
   data: PropTypes.shape(),
