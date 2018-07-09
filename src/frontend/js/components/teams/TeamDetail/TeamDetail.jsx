7128e0e1553a62ef110704fe6198701a1094fb87
Graham McCulloch <admin@grahammcculloch.co.uk>
2018-07-09 18:29:53 +0700

    Combine team results and fixtures into one table

--- src/frontend/js/components/teams/TeamDetail/TeamDetail.jsx
+++ src/frontend/js/components/teams/TeamDetail/TeamDetail.jsx
@@ -12,50 +12,33 @@ import SquadRosterWrapper from './SquadRosterWrapper';
  * Wrapper component for various components displayed for a particular club team (in a particular season).
  * 
  * Includes:
- * - Results
- * - Fixtures
+ * - Results/Fixtures
  * - League Table
  * - Squad Roster
  */
-const TeamDetail = ({ data, division, teamId, teamGenderlessName, seasonId }) => {
-  const results = [];
-  const fixtures = [];
-  data.results.forEach((match) => {
-    if (Match.isPast(match)) results.push(match);
-    else fixtures.push(match);
-  });
-  return (
-    <Accordion multiselectable accordionId="team-details">
-      <AccordionCard cardId="results" accordionId="team-details" title="Results">
-        {results.length > 0 ? (
-          <MatchData matches={results} exclude={[MatchItem.ourTeam]} fillBlankSaturdays />
-        ) : (
-          <p className="lead g-font-style-italic">No matches played</p>
-        )}
-      </AccordionCard>
-      <AccordionCard cardId="fixtures" accordionId="team-details" title="Fixtures">
-        {fixtures.length > 0 ? (
-          <MatchData
-            matches={fixtures}
-            fillBlankSaturdays
-            exclude={[MatchItem.ourTeam, MatchItem.result, MatchItem.scorers, MatchItem.awards]}
-            priorities={{ [MatchItem.time]: 1 }}
-          />
-        ) : (
-          <p className="lead g-font-style-italic">No upcoming fixtures</p>
-        )}
-      </AccordionCard>
-      {division.id && (
-        <AccordionCard cardId="league-table" accordionId="team-details" title="League Table">
-          <LeagueTable divisionId={division.id} seasonId={seasonId} teamName={teamGenderlessName} />
-        </AccordionCard>
+const TeamDetail = ({ data, division, teamId, teamGenderlessName, seasonId }) => (
+  <Accordion multiselectable accordionId="team-details">
+    <AccordionCard
+      cardId="results_fixtures"
+      accordionId="team-details"
+      title="Results &amp; Fixtures"
+    >
+      {data.results.length > 0 ? (
+        <MatchData matches={data.results} exclude={[MatchItem.ourTeam]} fillBlankSaturdays />
+      ) : (
+        <p className="lead g-font-style-italic">No matches</p>
       )}
-      <AccordionCard cardId="squad-roster" accordionId="team-details" title="Squad Roster">
-        <SquadRosterWrapper teamId={teamId} seasonId={seasonId} />
+    </AccordionCard>
+    {division.id && (
+      <AccordionCard cardId="league-table" accordionId="team-details" title="League Table">
+        <LeagueTable divisionId={division.id} seasonId={seasonId} teamName={teamGenderlessName} />
       </AccordionCard>
-    </Accordion>
-  );
-};
+    )}
+    <AccordionCard cardId="squad-roster" accordionId="team-details" title="Squad Roster">
+      <SquadRosterWrapper teamId={teamId} seasonId={seasonId} />
+    </AccordionCard>
+  </Accordion>
+);
 
 TeamDetail.propTypes = {
   data: PropTypes.shape(),
