ea57d5f57f09053ac5b6933651105870b1425bb0
Matt Kern <matt.kern@cyantechnology.com>
2018-11-26 21:15:20 +0000

    Read environment variable from file if present

--- src/cshc/wsgi.py
+++ src/cshc/wsgi.py
@@ -8,10 +8,18 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+import sys
 from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cshc.settings")
+
+for path in '../env.py', '../../env.py':
+    try:
+        with open(path) as envpy:
+            exec(envpy.read())
+    except FileNotFoundError:
+        pass
 
 #pylint: disable=C0103
 application = Sentry(get_wsgi_application())
