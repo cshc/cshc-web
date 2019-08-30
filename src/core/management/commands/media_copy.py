536afb1e5c65734ead43dd0376a193d1f183409b
Graham McCulloch <admin@grahammcculloch.co.uk>
2019-08-30 09:13:07 +0700

    Updated meda_copy management command
    Now just copies from the new prod bucket to the staging bucket (and local if requested)

--- src/core/management/commands/media_copy.py
+++ src/core/management/commands/media_copy.py
@@ -1,9 +1,8 @@
 """
 Management command that copies the media folder (uploads etc) from the production
 Amazon S3 bucket to one or more of the following destinations:
-1: --staging: the new staging media Amazon S3 bucket.
-2. --new-prod: the new production media Amazon S3 bucket.
-3. --local: the local media folder (in your dev environment)
+1: --staging: the staging media Amazon S3 bucket.
+2. --local: the local media folder (in your dev environment)
 
 Note that all copied files will have the ACL 'public-read'
 """
@@ -13,12 +12,10 @@ import traceback
 import boto3
 from django.core.management.base import BaseCommand
 
-OLD_PROD_BUCKET_NAME = 'cshc'
-NEW_PROD_BUCKET_NAME = 'cshc-v3'
-NEW_STAGING_BUCKET_NAME = 'cshc-staging-v3'
+PROD_BUCKET_NAME = 'cshc-v3'
+STAGING_BUCKET_NAME = 'cshc-staging-v3'
 
 extra_args = {'ACL': 'public-read'}
-
 
 class Command(BaseCommand):
 
@@ -33,47 +30,27 @@ class Command(BaseCommand):
             default=False,
             help='Copy to the local media folder as well',
         )
-        parser.add_argument(
-            '--new-prod',
-            action='store_true',
-            dest='new_prod',
-            default=False,
-            help='Copy to the new production media folder as well',
-        )
-        parser.add_argument(
-            '--staging',
-            action='store_true',
-            dest='staging',
-            default=False,
-            help='Copy to the new staging media folder as well',
-        )
 
     def handle(self, *args, **options):
         try:
             s3 = boto3.resource('s3')
-            old_prod_bucket = s3.Bucket(OLD_PROD_BUCKET_NAME)
-            new_bucket = None
+            prod_bucket = s3.Bucket(PROD_BUCKET_NAME)
+            staging_bucket = s3.Bucket(STAGING_BUCKET_NAME)
             new_objects = {}
 
-            if options['new_prod']:
-                new_bucket = s3.Bucket(NEW_PROD_BUCKET_NAME)
-
-            if options['staging']:
-                new_bucket = s3.Bucket(NEW_STAGING_BUCKET_NAME)
-
-            if new_bucket:
+            if staging_bucket:
                 new_objects = dict(
                     ((elt.key, elt.size), elt.last_modified)
-                    for elt in new_bucket.objects.filter(Prefix='media')
+                    for elt in staging_bucket.objects.filter(Prefix='media')
                 )
 
-            for obj_summary in old_prod_bucket.objects.filter(Prefix='media'):
+            for obj_summary in prod_bucket.objects.filter(Prefix='media'):
                 copy_source = {
-                    'Bucket': OLD_PROD_BUCKET_NAME,
+                    'Bucket': PROD_BUCKET_NAME,
                     'Key': obj_summary.key,
                 }
 
-                if new_bucket:
+                if staging_bucket:
                     ts = new_objects.get((obj_summary.key, obj_summary.size))
                     if ts is not None and ts >= obj_summary.last_modified:
                         print('Skipping {}'.format(obj_summary.key))
@@ -81,8 +58,8 @@ class Command(BaseCommand):
 
                 print('Copying {}'.format(obj_summary.key))
 
-                if new_bucket:
-                    new_bucket.copy(
+                if staging_bucket:
+                    staging_bucket.copy(
                         copy_source,
                         obj_summary.key,
                         ExtraArgs=extra_args,
@@ -92,7 +69,7 @@ class Command(BaseCommand):
                     if not os.path.isfile(obj_summary.key):
                         os.makedirs(os.path.dirname(
                             obj_summary.key), exist_ok=True)
-                        old_prod_bucket.download_file(
+                        prod_bucket.download_file(
                             obj_summary.key, obj_summary.key)
         except:
             traceback.print_exc()
