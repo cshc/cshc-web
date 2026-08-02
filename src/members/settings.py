a98f3392c921cf165a12943f937d7fcbf9a57432
Ben Lye <ben@lye.co.nz>
2026-08-02 13:23:14 +0100

    Use ClubInfo model for shirt number selection config

--- src/members/settings.py
+++ src/members/settings.py
@@ -5,19 +5,3 @@ from django.conf import settings
 MEMBERS_PHOTO_DIR = getattr(
     settings, 'MEMBERS_PHOTO_DIR', 'uploads/profile_pics')
 """ The directory where uploaded profile pictures should be stored (within MEDIA_URL) """
-
-MEMBERS_SHIRT_NUMBER_MAX = getattr(
-    settings, 'MEMBERS_SHIRT_NUMBER_MAX', 299)
-""" The maximum permitted shirt number """
-
-MEMBERS_SHIRT_NUMBER_LIMIT = getattr(
-    settings, 'MEMBERS_SHIRT_NUMBER_LIMIT', 24)
-""" The number of available shirt numbers to return """
-
-MEMBERS_SHIRT_NUMBER_INCLUDE_NOT_CURRENT = getattr(
-    settings, 'MEMBERS_SHIRT_NUMBER_INCLUDE_NOT_CURRENT', True)
-""" Whether to include non-current members when looking for available shirt numbers """
-
-MEMBERS_SHIRT_NUMBER_GRACE_PERIOD = getattr(
-    settings, 'MEMBERS_SHIRT_NUMBER_GRACE_PERIOD', 2)
-""" Number of seasons after which non-current members' assigned shirt numbers are considered for reuse """
